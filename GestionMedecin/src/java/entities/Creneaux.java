db5323a07c1c1a21521fe7361f99f3c9736a3048
Maxime CHAUVET <maxime.chauvet0@gmail.com>
2016-12-17 20:42:06 +0100

    create entities

---
+++ GestionMedecin/src/java/entities/Creneaux.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author maxim
+ */
+@Entity
+public class Creneaux {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column (name = "Debut")
+    private Date debut;
+    
+    @Column (name = "Fin")
+    private Date fin;
+
+    public Long getId() {
+        return id;
+    }
+
+    public Date getDebut() {
+        return debut;
+    }
+
+    public Date getFin() {
+        return fin;
+    }
+
+    public void setDebut(Date debut) {
+        this.debut = debut;
+    }
+
+    public void setFin(Date fin) {
+        this.fin = fin;
+    }
+}
---
+++ GestionMedecin/src/java/entities/Patient.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author maxim
+ */
+@Entity
+public class Patient {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column (name = "Nom")
+    private String nom;
+    
+    @Column (name = "Prenom")
+    private String prenom;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+}
